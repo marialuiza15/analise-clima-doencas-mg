05eb71128e2254bf44f39396808e33212f238f81
Pedro Simas <pedrosimas4@gmail.com>
2025-06-29 18:47:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
+
 from scripts.limpeza import *
 from scripts.features import *
 from scripts.modelagem import *
 
-# Caminhos
 CAMINHO_CLIMA = 'dados_clima_mg/'
 CAMINHO_SAUDE = 'dados_saude_mg/'
 
-# Clima
 clima_total, dfs_por_ano = unir_e_separar_clima(CAMINHO_CLIMA)
 
-# Une clima + saúde para cada ano
-df_geral = {ano: unindo_clima_saude(dfs_por_ano[ano], CAMINHO_SAUDE, ano)
-            for ano in range(2010, 2024)}
+df_clima_2010 = dfs_por_ano[2010]
+df_clima_2011 = dfs_por_ano[2011]
+df_clima_2012 = dfs_por_ano[2012]
+df_clima_2013 = dfs_por_ano[2013]
+df_clima_2014 = dfs_por_ano[2014]
+df_clima_2015 = dfs_por_ano[2015]
+df_clima_2016 = dfs_por_ano[2016]
+df_clima_2017 = dfs_por_ano[2017]
+df_clima_2018 = dfs_por_ano[2018]
+df_clima_2019 = dfs_por_ano[2019]
+df_clima_2020 = dfs_por_ano[2020]
+df_clima_2021 = dfs_por_ano[2021]
+df_clima_2022 = dfs_por_ano[2022]
+df_clima_2023 = dfs_por_ano[2023]
 
-# Aplica features e filtra desconhecidos
-def preparar(df):
-    df = engenharia_de_features(df)
-    return df[df['risco_obito'] != 'Desconhecido']
+df_geral_2010 = unindo_clima_saude(df_clima_2010, CAMINHO_SAUDE, 2010)
+df_geral_2011 = unindo_clima_saude(df_clima_2011, CAMINHO_SAUDE, 2011)
+df_geral_2012 = unindo_clima_saude(df_clima_2012, CAMINHO_SAUDE, 2012)
+df_geral_2013 = unindo_clima_saude(df_clima_2013, CAMINHO_SAUDE, 2013)
+df_geral_2014 = unindo_clima_saude(df_clima_2014, CAMINHO_SAUDE, 2014)
+df_geral_2015 = unindo_clima_saude(df_clima_2015, CAMINHO_SAUDE, 2015)
+df_geral_2016 = unindo_clima_saude(df_clima_2016, CAMINHO_SAUDE, 2016)
+df_geral_2017 = unindo_clima_saude(df_clima_2017, CAMINHO_SAUDE, 2017)
+df_geral_2018 = unindo_clima_saude(df_clima_2018, CAMINHO_SAUDE, 2018)
+df_geral_2019 = unindo_clima_saude(df_clima_2019, CAMINHO_SAUDE, 2019)
+df_geral_2020 = unindo_clima_saude(df_clima_2020, CAMINHO_SAUDE, 2020)
+df_geral_2021 = unindo_clima_saude(df_clima_2021, CAMINHO_SAUDE, 2021)
+df_geral_2022 = unindo_clima_saude(df_clima_2022, CAMINHO_SAUDE, 2022)
+df_geral_2023 = unindo_clima_saude(df_clima_2023, CAMINHO_SAUDE, 2023)
 
-df_treino = pd.concat([preparar(df_geral[ano]) for ano in range(2010, 2023)], ignore_index=True)
-df_teste = preparar(df_geral[2023])
+# A partir daqui, todos os dataframes estão organizados, com dados de clima e saude para cada ano.
 
-# Treinamento + Teste + Visualização
-modelo = treinar_e_testar(df_treino, df_teste)
+# resultado.to_csv("resultado_uniao_2010.csv", index=False, encoding="utf-8") # cCaso precise ver o df completo
+
+# Junta todos os anos de 2010 a 2022
+df_treino = pd.concat([
+    engenharia_de_features(df_geral_ano)
+    for df_geral_ano in [
+        df_geral_2010, df_geral_2011, df_geral_2012, df_geral_2013,
+        df_geral_2014, df_geral_2015, df_geral_2016, df_geral_2017,
+        df_geral_2018, df_geral_2019, df_geral_2020, df_geral_2021,
+        df_geral_2022
+    ]
+], ignore_index=True)
+
+# Teste com 2023
+df_teste = engenharia_de_features(df_geral_2023)
+print(df_teste)
+modelo_final = treinar_e_testar(df_treino, df_teste)
